4152113c5a399d45aa928c527b5b50c5ce003d40
xoaadil <aaadil2004@gmail.com>
2025-08-28 21:55:28 +0530

    Add TypeScript as dev dependency

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,17 +4,21 @@ import dotenv from "dotenv";
 import authRoute from "./routes/authRoute";
 import contentRoute from "./routes/contentRoute"
 import cors from "cors"
+
 const app = express();
 dotenv.config();
 const PORT=process.env.PORT || 2020;
+
 app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("home root");
 });
 
 app.use("/auth", authRoute);
 app.use("/content",contentRoute)
+
 app.listen(PORT, async () => {
   console.log("app is listing on "+PORT);
   await mongoose
